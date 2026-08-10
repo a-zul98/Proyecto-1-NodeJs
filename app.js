65ed2d13eb48d86a14068944d75e728cf5ea184d
a-zul98 <dlvazul98@gmail.com>
2026-08-09 23:56:13 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,4 @@
-/*const express = require("express");
-console.log ("puto")
 
-const mysql = require("mysql2");
-console.log("conetadoamysql")
-
-const conexion = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "tuPassword",
-    database: "inventario"
-});
-console.log("createconnection")
-
-const app = express();
- console.log ("puta")
-app.get("/", (req, res) => {
-    res.send("Hola");
-    console.log ("holaaa");
-});
-
-app.listen(3000);*/
-
-// 1. Importar librerías
 const express = require("express");
  console.log("importo_express")
 
@@ -32,21 +9,19 @@ console.log ("importo database.sql")
 const inserts = fs.readFileSync("./sql/inserts.sql", "utf8");
 console.log("importo inserts.sql")
 
+
 const mysql = require("mysql2"); 
  console.log("importo mysql")
 
 
-// 2. Crear la aplicación
 const app = express();
  console.log("creo la app")
 
 
-// 3. Permitir recibir JSON
 app.use(express.json());
   console.log("permitio json")
 
 
-// 4. Conectar a MySQL
 const conexion = mysql.createConnection({
     host: "localhost",
     user: "root",
@@ -54,7 +29,7 @@ const conexion = mysql.createConnection({
     multipleStatements: true
 });
  console.log("conecto a mysql")
-////esto me dijo chatgpete
+
 
 conexion.connect((err) => {
 
@@ -99,33 +74,6 @@ conexion.connect((err) => {
 });
 
 
-
-
-
- /* esto me parece q esta mal
-conexion.connect((err) => {
-    if (err) {
-        console.log("Error al conectar");
-        return;
-    }
-
-    console.log("Conectado a MySQL");
-});
-
-
-conexion.query(inserts, (err) => {
-
-    if (err) {
-        console.log ("error en esta parte ni idea")
-        console.error(err);
-        return;
-    }
-
-    console.log("Datos insertados correctamente.");
-
-});*/
-
-
-app.listen(3000, () => {
+ pp.listen(3000, () => {
 console.log("Servidor iniciado en el puerto 3000");
 });
