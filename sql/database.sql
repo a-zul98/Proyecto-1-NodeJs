512a90d796777ac5655d85cc2c80a2794a82626a
a-zul98 <dlvazul98@gmail.com>
2026-08-10 16:26:02 -0300

    Update database.sql

--- sql/database.sql
+++ sql/database.sql
@@ -1,27 +1,29 @@
+
 CREATE DATABASE IF NOT EXISTS inventario;
 USE inventario;
 
-
 DROP TABLE IF EXISTS productos;
-DROP TABLE IF EXISTS proveedores;
+DROP TABLE IF EXISTS subcategorias;
+DROP TABLE IF EXISTS marcas;
 DROP TABLE IF EXISTS categorias;
 DROP TABLE IF EXISTS usuarios;
 
+
 CREATE TABLE marcas (
     id VARCHAR (36) PRIMARY KEY NOT NULL,
-    nombre_marca VARCHAR(36) NOT NULL,    
-    email_proovedor VARCHAR(36)
+    nombre_marca VARCHAR(100) NOT NULL,    
+    email_proveedor VARCHAR(100)
 );
 
 CREATE TABLE categorias (
     id VARCHAR (36) PRIMARY KEY NOT NULL,
-    nombre_categoria VARCHAR(36) NOT NULL
+    nombre_categoria VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE subcategorias (
     id VARCHAR (36) PRIMARY KEY NOT NULL,
-    nombre_subcategoria VARCHAR(36) NOT NULL,
-    categoria_id INT NOT NULL,
+    nombre_subcategoria VARCHAR(100) NOT NULL,
+    categoria_id VARCHAR (36) NOT NULL,
     
     CONSTRAINT fk_subcategoria_categoria
         FOREIGN KEY (categoria_id) REFERENCES categorias(id)
@@ -30,20 +32,22 @@ CREATE TABLE subcategorias (
 
 CREATE TABLE usuarios (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(36) NOT NULL,
-    clave VARCHAR(36) NOT NULL,
+    nombre VARCHAR(10) NOT NULL,
+    clave VARCHAR(500) NOT NULL,
     rol ENUM('ADMIN','EMPLEADO') NOT NULL
 );
 
 CREATE TABLE productos (
     id VARCHAR (36) PRIMARY KEY NOT NULL,
-    nombre VARCHAR(36) NOT NULL,
-    precio INT NOT NULL,
+    nombre VARCHAR(100) NOT NULL,
+    precio DECIMAL(10,2) NOT NULL,
     stock INT NOT NULL,
-    marcas_id INT NOT NULL,
-    categoria_id INT NOT NULL,
-    subcategoria_id INT NOT NULL, 
-    CONSTRAINT fk_producto_marcas FOREIGN KEY (marcas_id) REFERENCES marcas(id) ON DELETE CASCADE,
-    CONSTRAINT fk_producto_categoria FOREIGN KEY (categoria_id) REFERENCES categorias(id) ON DELETE CASCADE,
-    CONSTRAINT fk_producto_subcategoria FOREIGN KEY (subcategoria_id) REFERENCES subcategorias(id) ON DELETE CASCADE
+  marcas_id VARCHAR(36) NOT NULL,
+    subcategoria_id VARCHAR(36) NOT NULL,
+    
+    CONSTRAINT fk_producto_marcas 
+        FOREIGN KEY (marcas_id) REFERENCES marcas(id) ON DELETE CASCADE,
+        
+    CONSTRAINT fk_producto_subcategoria 
+        FOREIGN KEY (subcategoria_id) REFERENCES subcategorias(id) ON DELETE CASCADE
 );
