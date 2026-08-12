b4a456e3d3df75b47e3bad723f9782ab70acfe2f
Azul de la Vega <dlvazul98@gmail.com>
2026-08-12 00:17:13 -0300

    Agrego ruta para historial

--- app.js
+++ app.js
@@ -1,34 +1,140 @@
 
 const express = require("express");
  console.log("importo_express")
-
 const fs = require("fs");
-
 const script = fs.readFileSync("./sql/database.sql", "utf8");
-console.log ("importo database.sql")
+ console.log ("database.sql importado")
 const inserts = fs.readFileSync("./sql/inserts.sql", "utf8");
-console.log("importo inserts.sql")
-
-
+ console.log("inserts.sql importado")
 const mysql = require("mysql2"); 
- console.log("importo mysql")
-
-
 const app = express();
- console.log("creo la app")
-
-
+ console.log("Servidor Creado")
 app.use(express.json());
-  console.log("permitio json")
-
-
 const conexion = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "azulcapa",
+    database: "inventario",
     multipleStatements: true
 });
- console.log("conecto a mysql")
+ console.log("Conectado a Host")
+
+
+function registrarCambio(productoId, campo, valorAnterior, valorNuevo) {
+
+    const sql = `
+        INSERT INTO historial
+        (producto_id, campo_modificado, valor_anterior, valor_nuevo)
+        VALUES (?, ?, ?, ?)
+    `;
+
+    conexion.query(
+        sql,
+        [productoId, campo, valorAnterior, valorNuevo],
+        (err) => {
+
+            if (err) {
+                console.error("Error al registrar historial:");
+                console.error(err);
+                return;
+            }
+            console.log("Cambio registrado en historial");
+        }
+    );
+}
+
+
+
+app.put("/productos/:id", (req, res) => {
+
+    const id = req.params.id;
+
+    const { nombre, precio, stock } = req.body;
+
+    const sqlBuscar = `
+        SELECT *
+        FROM productos
+        WHERE id = ?
+    `;
+
+     conexion.query(sqlBuscar, [id], (err, resultados) => {
+
+        if (err) {
+            console.error("Error al buscar producto:", err);
+
+            return res.status(500).json({
+                mensaje: "Error al buscar el producto"
+            });
+     }
+
+        if (resultados.length === 0) {
+
+            return res.status(404).json({
+                mensaje: "Producto no encontrado"
+            });
+        }
+
+        const productoAnterior = resultados[0];
+
+        const sqlActualizar = `
+            UPDATE productos
+            SET nombre = ?, precio = ?, stock = ?
+            WHERE id = ?
+        `;
+
+        conexion.query(
+            sqlActualizar,
+            [nombre, precio, stock, id],
+            (err) => {
+
+                if (err) {
+                    console.error("Error al actualizar producto:", err);
+
+                    return res.status(500).json({
+                        mensaje: "Error al modificar el producto"
+                    });
+                }
+
+                if (productoAnterior.nombre != nombre) {
+
+                    registrarCambio(
+                        id,
+                        "nombre",
+                        productoAnterior.nombre,
+                        nombre
+                    );
+                }
+
+                if (productoAnterior.precio != precio) {
+
+                    registrarCambio(
+                        id,
+                        "precio",
+                        productoAnterior.precio,
+                        precio
+                    );
+                }
+
+                if (productoAnterior.stock != stock) {
+
+                    registrarCambio(
+                        id,
+                        "stock",
+                        productoAnterior.stock,
+                        stock
+                    );
+                }
+
+                res.json({
+                    mensaje: "Producto modificado correctamente"
+                });
+
+            }
+        );
+
+    });
+
+});
 
 
 conexion.connect((err) => {
@@ -37,19 +143,18 @@ conexion.connect((err) => {
         console.error(err);
         return;
     }
-    console.log ("esta linea todo piola")
 
     console.log("Conectado a MySQL");
 
     conexion.query(script, (err) => {
 
         if (err) {
-            console.log ("error en el conection query script")
+            console.log ("Error en el conection query script")
             console.error(err);
             return;
         }
 
-        console.log("Base y tablas creadas");
+        console.log("Base de datos y tablas creadas");
 
         conexion.query(inserts, (err) => {
 
@@ -63,6 +168,7 @@ conexion.connect((err) => {
 
             console.log("Datos insertados");
 
+
             app.listen(3000, () => {
                 console.log("Servidor iniciado");
             });
@@ -74,6 +180,6 @@ conexion.connect((err) => {
 });
 
 
- pp.listen(3000, () => {
+app.listen(3000, () => {
 console.log("Servidor iniciado en el puerto 3000");
 });
